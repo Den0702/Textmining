11c2875b11383f371ac3ced0aa40b3c9b3fbf008
Den0702 <dzmats@gmail.com>
2020-03-17 16:55:37 +0100

    Dokonanie lematyzacji tekstów z korpusu raz zapis wyników do folderu wyjściowego results

--- scripts/script1.r
+++ scripts/script1.r
@@ -1,8 +1,9 @@
 #włączenie bibliotek
 library(tm)
-
+library(hunspell)
+library (stringr)
 #zmiana katalogu roboczego
-workDir <- "D:\\Polska\\Edukacja_w_Polsce\\Uniwersytet ekonomiczny\\Studia magisterskie\\Przetwarzanie jezyka naturalnego\\Textmining_copy"
+workDir <- "D:\\Polska\\Edukacja_w_Polsce\\Uniwersytet ekonomiczny\\Studia magisterskie\\Przetwarzanie jezyka naturalnego\\Textmining"
 setwd(workDir)
 
 #definicja katalogów projektu
@@ -52,11 +53,48 @@ corpus <- tm_map(corpus, removeWords, stoplist)
 corpus <- tm_map(corpus, stripWhitespace) # stripWhitespace jest na końcu, bo mogą zostać nadmiarowe "białe znaki"
 # bo funkcja removeWords zamienia usuwane przez siebie znaki na spacje
 
+remove_char <- content_transformer(
+  function(x, pattern, replacement) 
+    gsub(pattern, replacement, x)
+  )
 
+#usunięcie "em dash" i 3/4 z tekstów
+corpus <- tm_map(corpus, remove_char, intToUtf8(8722), "")
+corpus <- tm_map(corpus, remove_char, intToUtf8(190), "")
 
+#lematyzacja - sprowadzenie do formy
+polish <- dictionary(lang = "pl_PL")
 
+lemmatize <- function(text){
+  simple_text <- str_trim(as.character(text[1]))
+  parsed_text <- strsplit(simple_text, split = " ")
+  new_text_vec <- hunspell_stem(parsed_text[[1]], dict = polish)
+  for (i in 1:length(new_text_vec)){
+    if(length(new_text_vec[[i]]) == 0) new_text_vec[i] <- parsed_text[[1]][i]
+    if(length(new_text_vec[[i]]) > 1) new_text_vec[i] <- new_text_vec[[i]][1]
+  }
+  new_text <- paste(new_text_vec, collapse = " ")
+  return(new_text)
+}
 
+corpus <- tm_map(corpus, content_transformer(lemmatize))
+#bedziemy siegac metadanych w pliku
+cut_extentions <- function(document){
+  meta(document, "id") <- gsub(pattern = "\\.txt$", "", meta(document, "id"))
+  return(document)
+}
 
+corpus <- tm_map(corpus, cut_extentions)
+
+preprocessed_dir <- paste(
+  outputDir,
+  "\\",
+  "Literatura - streszczenia - przetworzone",
+  sep = ""
+)
+dir.create(preprocessed_dir, showWarnings = FALSE)
+writeCorpus(corpus, path = preprocessed_dir)
+#writeLines(as.character(corpus[[1]]))
 
 
 
