c65d0bf24603edbb737c69c80aac0b0bd07873cf
Den0702 <dzmats@gmail.com>
2020-04-08 12:50:47 +0200

    Update 08_04_2020

--- scripts/script_2.R
+++ scripts/script_2.R
@@ -17,7 +17,12 @@ dir.create(outputDir, showWarnings = FALSE)
 dir.create(workspaceDir, showWarnings = FALSE)
 
 #utworzenie korpusu dokumentów - chcemy do input dir dokleić ścieżkę dostępu, ale ta zmienna zawiera tylko adres
-corpusDir <- paste(inputDir, "\\",  "Literatura - streszczenia - przetworzone", sep = "")
+corpusDir <- paste(
+  inputDir, 
+  "\\",  
+  "Literatura - streszczenia - przetworzone", 
+  sep = ""
+  )
 
 #VCorpus - tworzy obiekt korpusu dokumentów
 corpus <- VCorpus(
@@ -46,7 +51,7 @@ dtmTfAll <- DocumentTermMatrix(corpus)
 tdmTfidfAll <- TermDocumentMatrix(
   corpus,
   control = list(
-    weighting = weightBin
+    weighting = weightTfIdf
   )
 )
 
@@ -93,11 +98,11 @@ tdmTfidfBoundsMatrix <- as.matrix(tdmTfidfBounds)
 dtmTfidfBoundsMatrix <- as.matrix(dtmTfidfBounds)
 
 #eksport macierzy do pliku .csv
-#matrixFile <- paste(
- # outputDir,
-  #"\\",
-  #"Literatura - streszczenia - przetworzone",
-  #sep = ""
-#)
-#write.table(tdmTfidfBoundsMatrix, file = matrixFile, sep = ";", dec = ",", col.names = NA)
+matrixFile <- paste(
+  outputDir,
+  "\\",
+  "Literatura - streszczenia - przetworzone",
+  sep = ""
+)
+write.table(tdmTfidfBoundsMatrix, file = matrixFile, sep = ";", dec = ",", col.names = NA)
 
